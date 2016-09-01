712b9818688fd534d2268068ddf831b97420cb80
nushio <muranushi@gmail.com>
2016-09-01 15:10:52 +0900

    Add benchmark results

--- run-test.sh
+++ run-test.sh
@@ -1,1 +1,1 @@
-OMP_NUM_THREADS=16 mpiexec ./build/mwd_kernel --nx 512 --ny 512 --nz 512 --nt 500 --target-kernel 1 --mwd-type 2 --target-ts 2
+OMP_NUM_THREADS=16 mpiexec -n 2 ./build/mwd_kernel --npy 2 --ny 1024 --nx 512 --nz 512 --nt 500 --target-kernel 1 --mwd-type 2 --target-ts 2
